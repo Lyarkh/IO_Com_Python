da16230daab5475af5a89e267240d0d53aa345ed
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-02-09 00:03:35 -0300

    convertendo e lendo arquivos json e pickle

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
+import contatos_utils
+
 try:
-    with open('src/data/contatos.csv', encoding='latin_1') as arquivo_contato:
-        for linha in arquivo_contato:
-            print(linha, end= '')
-            
+    # contatos = contatos_utils.csv_para_contatos('src/data/contatos.csv')
+    # contatos_utils.contato_para_pickle(contatos, 'src/data/contatos.p')
+
+    #contatos = contatos_utils.pickle_para_contato('src/data/contatos.p')
+    #contatos_utils.contatos_para_json(contatos, 'src/data/contatos.json')
+    contatos = contatos_utils.json_para_contatos('src/data/contatos.json')
+    
+    for contato in contatos:
+        print(f'{contato.id} - {contato.nome} - {contato.email}')
 except FileNotFoundError:
     print('Arquivo não encontrado')
