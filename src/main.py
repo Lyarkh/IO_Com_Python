afb7ab2e4ddfb3fa622a8abc3ebd04cf6f56e242
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-02-08 22:19:18 -0300

    utilizando o 'with' para gerenciamento de contexto

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
 try:
-    arquivo_contato = open('src/data/nao-existe.csv', encoding='latin_1')
-
-    for linha in arquivo_contato:
-        print(linha, end= '')
+    with open('src/data/contatos.csv', encoding='latin_1') as arquivo_contato:
+        for linha in arquivo_contato:
+            print(linha, end= '')
+            
 except FileNotFoundError:
     print('Arquivo não encontrado')
-finally:
-    arquivo_contato.close()
--- src/testa_concorrencia.py
+++ src/testa_concorrencia.py
@@ -1,11 +1,8 @@
-arquivo1 = open('src/data/contatos-escrito.csv', encoding='latin_1', mode='w')
-arquivo2 = open('src/data/contatos-escrito.csv', encoding='latin_1', mode='a')
-
 contato_carol = '11,Carol,carol@carol.com.br\n'
 contato_andreza = '12,Andreza, andreza@adreza.com.br\n'
 
-arquivo1.write(contato_carol)
-arquivo2.write(contato_andreza)
+with open('src/data/contatos-escrito.csv', encoding='latin_1', mode='w') as arquivo1:
+    arquivo1.write(contato_carol)
 
-arquivo1.close()
-arquivo2.close()
+with open('src/data/contatos-escrito.csv', encoding='latin_1', mode='a') as arquivo2:
+    arquivo2.write(contato_andreza)
